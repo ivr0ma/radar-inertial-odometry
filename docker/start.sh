5034a101e698ece830d8932ed3bd658b9d4c3cc4
ivr0ma <ivroma.ru@yandex.ru>
2023-07-06 23:05:28 +0300

    add required dependencies

--- docker/start.sh
+++ docker/start.sh
@@ -2,9 +2,7 @@
 
 docker_dir=$(dirname $0)
 
-docker run -it -d --rm \
-    --env="DISPLAY=$DISPLAY" \
-    --privileged \
+docker run -it --rm \
     --name radar \
     -v $(realpath $docker_dir)/../../:/home/ivr0ma/catkin_ws/src:rw \
     my-ros:latest
