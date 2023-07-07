bd032c4c9d9eb0d1d7d98cd8754b39ea88130eb4
Alexander Abramenko <mr.abramenko@yandex.ru>
2023-07-07 09:58:56 +0300

    update scripts in docker folder

--- docker/start.sh
+++ docker/start.sh
@@ -2,7 +2,8 @@
 
 docker_dir=$(dirname $0)
 
-docker run -it --rm \
+docker run -it --rm -d \
     --name radar \
+    --net "host" \
     -v $(realpath $docker_dir)/../../:/home/ivr0ma/catkin_ws/src:rw \
     my-ros:latest
