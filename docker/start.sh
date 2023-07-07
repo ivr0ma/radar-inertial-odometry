f77db28c618e2824f748563f399d946fce6b7947
ivr0ma <ivroma.ru@yandex.ru>
2023-07-07 17:48:32 +0300

    fix some bugs

--- docker/start.sh
+++ docker/start.sh
@@ -3,6 +3,10 @@
 docker_dir=$(dirname $0)
 
 docker run -it --rm -d \
+    $ARGS \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --privileged \
     --name radar \
     --net "host" \
     -v $(realpath $docker_dir)/../../:/home/ivr0ma/catkin_ws/src:rw \
